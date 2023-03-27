4626569f50fb2b53e2d9354006a734bd8627e5c1
Luciano <cybergenik@gmail.com>
2023-03-27 16:13:53 -0600

    fix byte conversions

--- main.go
+++ main.go
@@ -41,6 +41,12 @@ func traverse(file string, recChan chan int64) {
 	recChan <- total
 }
 
+const (
+	KILOBYTE = 1024
+	MEGABYTE = 1_048_576
+	GIGABYTE = 1_073_741_824
+)
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "Godex: No arg provided, for usage do: --help")
@@ -64,14 +70,14 @@ func main() {
 		} else {
 			total_size = file.Size()
 		}
-		if total_size < 1000.0 { // bytes
+		if total_size < KILOBYTE { // bytes
 			fmt.Printf("%v size: %d\n", file.Name(), total_size)
-		} else if total_size < 1_000_000 { // kilobyte
-			fmt.Printf("%v size: %.1fK\n", file.Name(), float64(total_size)/1_000.0)
-		} else if total_size < 1_000_000_000 { // megabyte
-			fmt.Printf("%v size: %.1fMb\n", file.Name(), float64(total_size)/1_000_000.0)
+		} else if total_size < MEGABYTE { // kilobyte
+			fmt.Printf("%v size: %.1fK\n", file.Name(), float64(total_size)/KILOBYTE)
+		} else if total_size < GIGABYTE { // megabyte
+			fmt.Printf("%v size: %.1fMb\n", file.Name(), float64(total_size)/MEGABYTE)
 		} else {
-			fmt.Printf("%v size: %.1fGb\n", file.Name(), float64(total_size)/1_000_000_000.0)
+			fmt.Printf("%v size: %.1fGb\n", file.Name(), float64(total_size)/GIGABYTE)
 		}
 	}
 }
